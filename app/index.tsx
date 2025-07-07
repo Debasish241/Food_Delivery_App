3f573f33bb86cd8c2335328ec53d6f435b461b6e
Debasish Debnath <ddebnath241@gmail.com>
2025-07-07 16:35:26 +0530

    feat: add Quicksand font family and update Tailwind CSS configuration

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,10 @@ import "./global.css";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text
+        className="text-5xl text-center text-blue-500"
+        style={{ fontFamily: "QuickSand-Bold" }}
+      >
         Welcome to Nativewind!
       </Text>
     </View>
