567a02535b28593bfbf05ff66874041b79a871b4
Debasish Debnath <ddebnath241@gmail.com>
2025-07-09 16:13:34 +0530

    feat: integrate images and enhance FlatList header with delivery info

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,40 @@
-import { offers } from "@/constants";
+import { images, offers } from "@/constants";
 import cn from "clsx";
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "./global.css";
 export default function Index() {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white">
       <FlatList
         data={offers}
+        contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5">
+            <View className="flex-start ">
+              <Text className="small-bold text-primary ">DELIVERY TO</Text>
+              <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">
+                  Kolkata,India
+                </Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+            <Text>Cart</Text>
+          </View>
+        )}
         renderItem={({ item, index }) => {
           const isEven = index % 2 === 0;
           return (
@@ -19,6 +45,7 @@ export default function Index() {
                   isEven ? "flex-row-reverse" : "flex-row"
                 )}
                 style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#fffff22" }}
               >
                 {({ pressed }) => (
                   <Fragment>
@@ -29,8 +56,21 @@ export default function Index() {
                         resizeMode={"contain"}
                       />
                     </View>
-                    <View className="offer-card__info">
-                      <Text>{item.title}</Text>
+                    <View
+                      className={cn(
+                        "offer-card__info",
+                        isEven ? "pl-10" : "pr-10"
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor="#ffffff"
+                      />
                     </View>
                   </Fragment>
                 )}
