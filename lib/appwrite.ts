efbf8b59d2b20b8d04c2eed656208468dc426280
Debasish Debnath <ddebnath241@gmail.com>
2025-07-11 15:43:48 +0530

    feat: implement authentication screens with custom input and button components, enhance layout with images

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteCoonfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
   platform: "com.debasish.foodordering",
+  databaseId: "686f721e00185637f870",
+  userCollectionId: "686f7239002249ae26ee",
 };
